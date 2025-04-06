6d7e57f328e7516023f21b4767a30cd43f492cea
Hoang Tien Lang <langhoangtien@gmail.com>
2025-04-06 13:29:55 +0700

    update tranhdongho

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,8 @@ export type ConfigValue = {
 type BranchType = "quitmood" | "optilife";
 // ----------------------------------------------------------------------
 export const API_URL = import.meta.env.VITE_SERVER_URL ?? "";
-export const BRANCH: BranchType = import.meta.env.VITE_BRANCH ?? "quitmood"; // e.g. "main" or "dev"
+export const BRANCH: BranchType =
+  import.meta.env.VITE_BRANCH ?? "langtranhdongho"; // e.g. "main" or "dev"
 export const PAYPAL_CLIENT_ID = import.meta.env.VITE_PAYPAL_CLIENT_ID ?? "";
 export const CONFIG: ConfigValue = {
   appName: packageJson.name,
@@ -19,26 +20,16 @@ export const CONFIG: ConfigValue = {
   serverUrl: API_URL,
 };
 export const SETTING = {
-  quitmood: {
-    PRODUCT_NAME: "QuitMood ™",
-    COMPANY_NAME: "Quit Mood",
-    CONTACT_EMAIL: "contact@quitmood.net",
-    CONTACT_PHONE: "+1 302 590 6135",
-    CONTACT_ADDRESS: "1111B S Governors Ave STE 28573 Dover Daleware 19904",
-    CONTACT_WEBSITE: "https://www.quitmood.net",
-  },
-  optilife: {
-    PRODUCT_NAME: "OptiLife ™",
-    COMPANY_NAME: "OptiLife",
-    CONTACT_EMAIL: "contact@optilifecompany.com",
-    CONTACT_PHONE: "+1 302 590 6135",
-    CONTACT_ADDRESS: "1111B S Governors Ave STE 28573 Dover Daleware 19904",
-    CONTACT_WEBSITE: "https://www.optilifecompany.com",
-  },
+  PRODUCT_NAME: "QuitMood ™",
+  COMPANY_NAME: "Quit Mood",
+  CONTACT_EMAIL: "contact@quitmood.net",
+  CONTACT_PHONE: "+1 302 590 6135",
+  CONTACT_ADDRESS: "1111B S Governors Ave STE 28573 Dover Daleware 19904",
+  CONTACT_WEBSITE: "https://www.quitmood.net",
 };
-export const PRODUCT_NAME = SETTING[BRANCH].PRODUCT_NAME;
-export const COMPANY_NAME = SETTING[BRANCH].COMPANY_NAME;
-export const CONTACT_EMAIL = SETTING[BRANCH].CONTACT_EMAIL;
-export const CONTACT_PHONE = SETTING[BRANCH].CONTACT_PHONE;
-export const CONTACT_ADDRESS = SETTING[BRANCH].CONTACT_ADDRESS;
-export const CONTACT_WEBSITE = SETTING[BRANCH].CONTACT_WEBSITE;
+export const PRODUCT_NAME = SETTING.PRODUCT_NAME;
+export const COMPANY_NAME = SETTING.COMPANY_NAME;
+export const CONTACT_EMAIL = SETTING.CONTACT_EMAIL;
+export const CONTACT_PHONE = SETTING.CONTACT_PHONE;
+export const CONTACT_ADDRESS = SETTING.CONTACT_ADDRESS;
+export const CONTACT_WEBSITE = SETTING.CONTACT_WEBSITE;
